82ab59c87e6c07918e9c6548b22abde692411d6b
SUMIT_DEVDA <113769249+Callmesumit@users.noreply.github.com>
2025-05-09 10:27:56 +0530

    Update authSlice.js

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,1 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+// const { User } = require("lucide-react");
+//import {User} from "lucide-react"
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        user:null
+    },
+    reducers:{
+        //action
+        setUser:(state, action) => {
+            state.user = action.payload;
+        }
+    }
+});
 
+export const {setUser} = authSlice.actions;
+export default authSlice.reducer;
